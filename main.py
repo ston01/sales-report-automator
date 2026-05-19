6964aff297ce6019e43021dbb44176695cef0d98
Emanuel de Carvalho Terra <emanuelt783@gmail.com>
2026-05-19 15:57:50 -0300

    feat: ENTER para sair
    - adicionado o input final pro programa nao fechar sozinho, mas sim apenas se o usuario digitar ENTER

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from lib.executor import executar
 
 if __name__ == "__main__":
     executar()
+try:
+    input("\nPressione ENTER para sair...")
+except KeyboardInterrupt:
+    print("\033[313\nPrograma encerrado pelo usuário.\033[0m")
