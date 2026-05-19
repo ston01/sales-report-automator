52b28d4303272b6ad540819c59eb84e9665c2226
Emanuel de Carvalho Terra <emanuelt783@gmail.com>
2026-05-19 12:10:55 -0300

    feat: prevenção de erros
    - Criado uma persistencia na leitura da planilha, pra se caso o usuário digitar o nome de uma planilha a qual não exista na pasta "/entrada" o programa persiste ate o usuário digitar corretamente ou quiser sair do programa
    - criada a função limpa_tela() pra limpar a tela assim que o usuario errar, assim evitando a poluição no terminal
    - adicionada a confirmação com a tecla ENTER para continuar o programa após o erro

--- lib/leitor.py
+++ lib/leitor.py
@@ -1,18 +1,39 @@
 from pathlib import Path
 import pandas as pd
+import os
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def ler_planilha(nome_arquivo: str):
     pasta_entrada = Path("data/entrada")
     caminho_arquivo = pasta_entrada / nome_arquivo
     if not caminho_arquivo.exists():
-        raise FileNotFoundError(f"Arquivo {caminho_arquivo} não encontrado.")
+        raise FileNotFoundError(f"\nArquivo {caminho_arquivo} não encontrado.")
     df = pd.read_excel(caminho_arquivo)
     return df
 
 
 def ler_dados():
-    nome_arquivo = input("Nome da planilha: ")
-    df = ler_planilha(nome_arquivo)
-    print("Planilha carregada com sucesso!\n")
-    print(df.head())
-    return df
+    while True:
+        try:
+            nome_arquivo = input("Nome da planilha: ")
+            df = ler_planilha(nome_arquivo)
+            print("\nPlanilha carregada com sucesso!\n")
+            print(df.head())
+            return df
+        except FileNotFoundError as e:
+            print(e)
+            input("Pressione ENTER para tentar novamente...")
+            limpar_tela()
+        except KeyboardInterrupt:
+            print("\nProcesso interrompido pelo usuário.")
+            exit()
+        except EOFError:
+            print("\nEntrada finalizada pelo usuário.")
+            exit()
+        except Exception as e:
+            print(f"Erro ao ler a planilha: {e}")
+            input("Pressione ENTER para tentar novamente...")
+            limpar_tela()
